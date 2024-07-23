c2c5f1ab2ad6b505d731dff1e40e32923fdb04e0
unknown <smuscolo2003@gmail.com>
2024-07-23 20:54:54 -0300

    Fix/svgs

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
+import { GraphicCard, Octocat } from "../Icons";
 
 function Navbar(){
     return(
         <header>
             <nav className="flex items-center justify-between pt-4 pb-4 border-b-[0.5px] border-[#B94CED]">
-                <Image src={'/graphic-card.svg'} width={50} height={50} alt='graphic card logo' className="ml-6" />
-                <a href="https://github.com/villarrealjoaquin/PartitureAI" target="_blank" rel="noopener noreferrer"><Image src={'/octocat.svg'} width={50} height={50} alt='github octocat' className="mr-6" /></a>
+                <GraphicCard className={'ml-6'} width="50px" height="50px" />
+                <a href="https://github.com/villarrealjoaquin/PartitureAI" target="_blank" rel="noopener noreferrer"><Octocat className={'mr-6'} width="50px" height="50px" /></a>
             </nav>
         </header>
     )
