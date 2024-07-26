3d73f1314696987f7a6855da9a34df7c26327c2d
villarrealjoaquin <villarrealjoaquin09@gmail.com>
2024-07-26 20:26:33 -0300

    feat: add Husky configuration for pre-commit hooks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import Hero from "@/components/Home/Hero/Hero";
 
 export default function Home() {
-
   return (
-      <main className="flex flex-col items-center flex-grow pb-28">
-        <Hero />
-      </main>
+    <main className="flex flex-col items-center flex-grow pb-28">
+      <Hero />
+    </main>
   );
 }
