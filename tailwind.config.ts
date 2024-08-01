188b7493028f867febc1d3e0e27fcbfb871f5fd7
unknown <smuscolo2003@gmail.com>
2024-07-31 21:27:11 -0300

    Feature/added animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,10 +27,15 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "infinite-scroll": {
+          from: { transform: "translateY(0)" },
+          to: { transform: "translateY(-100%)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "infinite-scroll": "infinite-scroll 20s linear infinite",
       },
     },
   },
